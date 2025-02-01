6c701f7bf079ecebdb9df9a0f3a01da2a32bd3ad
mirko93 <skopljak.mirko93@gmail.com>
2025-02-01 14:29:31 +0100

    test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mirko93.github.io',
-  base: '/',
+  base: './dist',
   output: "static",
   devToolbar: {
     enabled: false
